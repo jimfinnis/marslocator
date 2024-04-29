bedb02648b3d8c22367a5748433a27ce9464b63f
Jim Finnis <jim.finnis@gmail.com>
2024-04-29 12:10:12 +0100

    map processing code to generate contour map

--- equirect/Equirectangular/process.py
+++ equirect/Equirectangular/process.py
@@ -15,16 +15,28 @@ def process(planet):
     hmname = f"{planet}-equirect-hm"
     terrname = f"{planet}-equirect"
     
+    print(f"Reading planet {planet}")
+    
     hm = imread(hmname)
     terr = imread(terrname)
     
-    print(hm.shape)
-    print(terr.shape)
-    
+    print(f"heightmap: {hm.shape}, terrain: {terr.shape}")
     
     # convert height map to single channel
     hm = cv.cvtColor(hm,cv.COLOR_BGR2GRAY)
     
+    # multiply by heightmap
+    hm2 = hm.astype(np.float32) / 255.0
+    hm2 = hm2**0.2
+    heightDarkAmount = 0.8
+    hm2 = (1.0-heightDarkAmount) + hm2*heightDarkAmount
+    hm2 = np.dstack([hm2, hm2, hm2])
+    
+    terr = terr*hm2
+    terr = cv.normalize(terr,terr,0,255,cv.NORM_MINMAX)
+    print(np.min(terr),np.max(terr))
+    
+    # determine contour levels
     heights = np.linspace(0,255,numcontours)
     heights = [int(x) for x in heights[1:-1]]
     
@@ -32,14 +44,19 @@ def process(planet):
     for idx, i in enumerate(heights):
         _, thresh = cv.threshold(hm, i, 255, cv.THRESH_BINARY)
         cons,heir =  cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        print(type(cons))
         showThickContour = ((idx-1)%thickevery)==0
         cv.drawContours(terr, cons, -1, (0,0,0),
             2 if showThickContour else 1
         )
     
-    cv.imwrite("foo.png",terr)
+    cv.imwrite(f"{planet}-Contours.png",terr)
         
     
 
-process("Titan")
+def processall():
+    for x in ["Alien","Earthlike","Europa","Mars","Moon","Pertam",
+        "Titan","Triton"]:
+        process(x)
+
+
+processall()
